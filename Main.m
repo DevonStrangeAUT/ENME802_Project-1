e7f05b1396084846c7464bbfe8f9839891a21c27
Devon Strange <why0852@autuni.ac.nz>
2026-04-21 11:00:27 +1200

    Updated to handle strains

--- Main.m
+++ Main.m
@@ -1,4 +1,5 @@
 [data, F, dzero, n_nodes, n_element] = build_tool_data();
+
 % extract variables from data
 E = data(1,8);
 A = data(1,9);
@@ -8,24 +9,29 @@ Y = data(1:n_nodes,7);
 NDU = data(1,11);
 v = data(1,13);
 t = data(1,14);
+
 % initiate Matrices
 KE = zeros(6);
 K = zeros(2*n_nodes);
+
 % main Routine
 % loop over elements: form element stiffness and assemble global stiffness
 for i=1:n_element
     % evaluates Elemental Stiffness Matrices
     [KE] = pre_processing(i,ncon,X,Y,E,A,t,v);
+
     % assembles Overall Stiffness Matrix
     n1 = ncon(i,1);
     n2 = ncon(i,2);
     n3 = ncon(i,3);
+
     ROC(1) = (2*n1)-1;
     ROC(2) = (2*n1);
     ROC(3) = (2*n2)-1;
     ROC(4) = (2*n2);
     ROC(5) = (2*n3)-1;
     ROC(6) = (2*n3);
+
     % map element DOFs to global DOFs and add element KE
     for IX = 1:6
         MI = ROC(IX);
@@ -35,15 +41,21 @@ for i=1:n_element
         end
     end
 end
+
 KM = K;
+
 % calculates Unknown Displacements and Stresses
 [U, Sx, Sy, Sxy] = post_processing(n_element,KM,NDU,dzero,F,ncon,X,Y,E,A,v);
-% outputs Exported To Excel Files
-dlmwrite('Displacement.xls',U,'')
-dlmwrite('StressX.xls',Sx,'')
-dlmwrite('StressY.xls',Sy,'')
-dlmwrite('StressXY.xls',Sxy,'')
+
+% Outputs Exported To Excel Files
+dlmwrite('Displacement.xls', U,   '')
+dlmwrite('StressX.xls',      Sx,  '')
+dlmwrite('StressY.xls',      Sy,  '')
+dlmwrite('StressXY.xls',     Sxy, '')
+dlmwrite('StrainX.xls',      Ex,  '')
+dlmwrite('StrainY.xls',      Ey,  '')
+dlmwrite('StrainXY.xls',     Gxy, '')
+
 % Plots the Initial and Final Structure
-display_structure(n_element,ncon,X,Y,U,F,dzero);
-% plots the Initial and Final Structure
-display_structure(n_element,ncon,X,Y,U,F);
+display_structure(n_element, ncon, X, Y, U, F, dzero);
+
--- run_FEA.m
+++ run_FEA.m
@@ -1,6 +1,6 @@
-function [U,Sx,Sy,Sxy] = run_FEA(ncon,X,Y,E,v,t,F,dzero,NDU)
+function [U, Sx, Sy, Sxy, Ex, Ey, Gxy] = run_FEA(ncon, X, Y, E, v, t, F, dzero, NDU)
 % RUN_FEA - Assemble global stiffness and run post-processing
-%
+
 % Input arguments:
 % ncon   - element connectivity (n_element x nodes_per_element)
 % X,Y    - nodal coordinates
@@ -8,7 +8,7 @@ function [U,Sx,Sy,Sxy] = run_FEA(ncon,X,Y,E,v,t,F,dzero,NDU)
 % F      - global force vector
 % dzero  - prescribed displacements/conditions
 % NDU    - number of degrees of freedom
-%
+
 % Output arguments:
 % U      - displacement solution
 % Sx,Sy  - stress components
@@ -17,13 +17,16 @@ function [U,Sx,Sy,Sxy] = run_FEA(ncon,X,Y,E,v,t,F,dzero,NDU)
 n_nodes = length(X);
 n_element = size(ncon,1);
 K = zeros(2*n_nodes);
-% === Assemble stiffness ===
+
+% Assemble stiffness
 % Loop over elements, compute element stiffness and scatter into K
 for i = 1:n_element
     KE = pre_processing(i,ncon,X,Y,E,[],t,v);
+
     n1 = ncon(i,1);
     n2 = ncon(i,2);
     n3 = ncon(i,3);
+
     ROC = [2*n1-1 2*n1 2*n2-1 2*n2 2*n3-1 2*n3];
     for IX = 1:6
         for JX = 1:6
@@ -31,8 +34,9 @@ for i = 1:n_element
         end
     end
 end
-% === Solve ===
-% Apply boundary conditions and compute displacements and stresses
-[U,Sx,Sy,Sxy] = post_processing(...
-    n_element,K,NDU,dzero,F,ncon,X,Y,E,[],v);
+
+% Solve
+% Apply boundary conditions and compute displacements, stresses and strains
+[U, Sx, Sy, Sxy, Ex, Ey, Gxy] = post_processing(...
+    n_element, K, NDU, dzero, F, ncon, X, Y, E, [], v);
 end
